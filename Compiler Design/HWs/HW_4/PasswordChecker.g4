8d05a91ebc5a7b53e56c96c08f07f2b477cf6177
Alireza Mahdizadeh <A.L.Mahdizadeh@ut.ac.ir>
2023-12-13 00:43:32 +0330

    Added Compier Design HW_4

---
+++ Compiler Design/HWs/HW_4/PasswordChecker.g4
@@ -0,0 +1,15 @@
+// PasswordChecker
+grammar PasswordChecker;
+
+// Parser rules
+startRule : password EOF;
+
+password : (PASSWORD_VALID | PASSWORD_INVALID);
+
+// Lexer rules
+PASSWORD_VALID : 'true';
+PASSWORD_INVALID : 'false';
+
+// Skip whitespace
+WS : [ \t\r\n]+ -> skip;
+
---
+++ Compiler Design/HWs/HW_4/PasswordCheckerBaseListener.java
@@ -0,0 +1,63 @@
+// Generated from PasswordChecker.g4 by ANTLR 4.13.1
+
+import org.antlr.v4.runtime.ParserRuleContext;
+import org.antlr.v4.runtime.tree.ErrorNode;
+import org.antlr.v4.runtime.tree.TerminalNode;
+
+/**
+ * This class provides an empty implementation of {@link PasswordCheckerListener},
+ * which can be extended to create a listener which only needs to handle a subset
+ * of the available methods.
+ */
+@SuppressWarnings("CheckReturnValue")
+public class PasswordCheckerBaseListener implements PasswordCheckerListener {
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterStartRule(PasswordCheckerParser.StartRuleContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitStartRule(PasswordCheckerParser.StartRuleContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterPassword(PasswordCheckerParser.PasswordContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitPassword(PasswordCheckerParser.PasswordContext ctx) { }
+
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterEveryRule(ParserRuleContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitEveryRule(ParserRuleContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void visitTerminal(TerminalNode node) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void visitErrorNode(ErrorNode node) { }
+}
